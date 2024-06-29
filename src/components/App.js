176f41de07ef5cace28caef01ed5604c8363d1ba
Victor Wong <=>
2024-06-29 12:59:23 -0700

    init & tested storefront api is working

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      headless
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ShopProvider from './context/shopContext'
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
+  <ShopProvider>
     <App />
-  </React.StrictMode>
+  </ShopProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
